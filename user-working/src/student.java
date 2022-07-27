97e9eb350db1a1f075cedce92e68947fcc96844a
Your Name <you@example.com>
2022-07-27 11:16:28 +0800

    wulun

---
+++ user-working/src/student.java
@@ -0,0 +1,27 @@
+public class student {
+
+    public String StudentNo;
+    public String StudentName;
+    public int studentAge;
+    public String StudentSex;
+    public String StudentScore;
+    public int StudentWeight;
+
+    public void bubbleLibrary(){
+        System.out.println("正在图书馆摸鱼……");
+    }
+    public void upClass(){
+        System.out.println("在课上摸鱼");
+    }
+    public void doBroadcastExercise(){
+        System.out.println("在操场上摸鱼");
+    }
+    public void standInTheCorridor(){
+        System.out.println("被罚站了");
+    }
+
+}
+
+
+
+}
